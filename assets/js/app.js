f7407fbd4d8c5aaaceb506bc9e4c6b645e5461fb
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-07 17:56:07 -0400

    Agregado detalle.html y arreglando duplicados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 
     // Multiplicar cards sin repetir HTML
-    const contenedor = document.getElementById('mi-lista');
+    const contenedor = document.getElementById('lugares-clima');
     const lugares = [
-        {nombre: "Tu ciudad", clima: "18" },
-        {nombre: "Osorno, Chile", clima: "7" },
-        {nombre: "Reñaca, Chile", clima: "15" },
-        {nombre: "Panguipulli, Chile", clima: "10" },
-        {nombre: "Toronto, Canadá", clima: "10" },
-        {nombre: "Londres, Inglaterra", clima: "20" },
-        {nombre: "Melbourne, Australia", clima: "25" }
+        {nombre: "Tu ciudad", clima: "18", url: "/detalle.html#1", lunes: "20", martes: "16" },
+        {nombre: "Osorno, Chile", clima: "7", url: "/detalle.html#2", lunes: "20", martes: "16" },
+        {nombre: "Reñaca, Chile", clima: "15", url: "/detalle.html#3", lunes: "20", martes: "16" },
+        {nombre: "Panguipulli, Chile", clima: "10", url: "/detalle.html#4", lunes: "20", martes: "16" },
+        {nombre: "Toronto, Canadá", clima: "10", url: "/detalle.html#5", lunes: "20", martes: "16" },
+        {nombre: "Londres, Inglaterra", clima: "20", url: "/detalle.html#6", lunes: "20", martes: "16" },
+        {nombre: "Melbourne, Australia", clima: "25", url: "/detalle.html#7", lunes: "20", martes: "16" }
     ];
 
     contenedor.innerHTML = '';
@@ -17,15 +17,15 @@ $(document).ready(function () {
     lugares.forEach(lugar => {
         contenedor.innerHTML += `
             <article class="card col-sm-12 col-md-4 mb-3">
-                <img src="assets/img/prueba.png" class="card-img-top" alt="Imagen de Lugar">
+                
                 <div class="card-body">
                     <h5 class="card-title">${lugar.nombre}</h5>
                     <p class="card-text">${lugar.clima}</p>
-                    <a href="#" class="btn btn-primary col-12">Detalles</a>
+                    <img src="assets/img/prueba.png" class="card-img-top mt-3" alt="Imagen de Lugar">
+                    <a type="button" class="btn btn-primary class-12" href="${lugar.url}">Detalles</a>
                 </div>
             </article>
         `;
     });
 
-    
 });
