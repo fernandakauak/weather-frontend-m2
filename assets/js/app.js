5025b111d3aabe3ce4f76c6dc7366b6f4022a899
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-07 23:45:26 -0400

    Finaliza implementación interacción detalle y se agrega su html correspondiente

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,17 @@ $(document).ready(function () {
     /* MULTIPLICAR CARD SIN REPETIR HTML */
     const contenedor = document.getElementById('lugares-clima');
     const lugares = [
-        { num: "1", nombre: "Santiago, Chile", ico: "☁️", clima: "18°C", lunes: "18°C", martes: "20°C", miercoles: "16°C", jueves: "18°C", viernes: "20°C", sabado: "16°C", domingo: "16°C" },
-        { num: "2", nombre: "Osorno, Chile", ico: "🌧️", clima: "7°C", lunes: "20", martes: "16" },
-        { num: "3", nombre: "Reñaca, Chile", ico: "⛅", clima: "15°C", lunes: "20", martes: "16" },
-        { num: "4", nombre: "Panguipulli, Chile", ico: "🌧️", clima: "10°C", lunes: "20", martes: "16" },
-        { num: "5", nombre: "Toronto, Canadá", ico: "⛅", clima: "10°C", lunes: "20", martes: "16" },
-        { num: "6", nombre: "Londres, Inglaterra", ico: "🌧️", clima: "20°C", lunes: "20", martes: "16" },
-        { num: "7", nombre: "Melbourne, Australia", ico: "☀️", clima: "25°C", lunes: "20", martes: "16" }
+        { num: "1", nombre: "Santiago, Chile", ico: "☁️", clima: "18°C", martes: "20°C", miercoles: "16°C", jueves: "18°C", viernes: "20°C", sabado: "16°C", domingo: "16°C" },
+        { num: "2", nombre: "Osorno, Chile", ico: "🌧️", clima: "7°C", martes: "5°C", miercoles: "8°C", jueves: "8°C", viernes: "9°C", sabado: "5°C", domingo: "7°C" },
+        { num: "3", nombre: "Reñaca, Chile", ico: "⛅", clima: "15°C", martes: "18°C", miercoles: "16°C", jueves: "13°C", viernes: "10°C", sabado: "15°C", domingo: "16°C" },
+        { num: "4", nombre: "Panguipulli, Chile", ico: "🌧️", clima: "10°C", martes: "8°C", miercoles: "10°C", jueves: "9°C", viernes: "7°C", sabado: "6°C", domingo: "7°C" },
+        { num: "5", nombre: "Toronto, Canadá", ico: "⛅", clima: "10°C", martes: "13°C", miercoles: "16°C", jueves: "12°C", viernes: "15°C", sabado: "16°C", domingo: "15°C" },
+        { num: "6", nombre: "Londres, Inglaterra", ico: "🌧️", clima: "20°C", martes: "21°C", miercoles: "18°C", jueves: "17°C", viernes: "19°C", sabado: "16°C", domingo: "19°C" },
+        { num: "7", nombre: "Melbourne, Australia", ico: "☀️", clima: "25°C", martes: "27°C", miercoles: "26°C", jueves: "28°C", viernes: "23°C", sabado: "26°C", domingo: "25°C" },
+        { num: "8", nombre: "Kioto, Japón", ico: "☀️", clima: "20°C", martes: "21°C", miercoles: "20°C", jueves: "23°C", viernes: "22°C", sabado: "20°C", domingo: "21°C" },
+        { num: "9", nombre: "Málaga, España", ico: "🌥️", clima: "21°C", martes: "23°C", miercoles: "22°C", jueves: "23°C", viernes: "25°C", sabado: "20°C", domingo: "21°C" },
+        { num: "10", nombre: "Nueva York, EEUU", ico: "☁️", clima: "10°C", martes: "11°C", miercoles: "10°C", jueves: "13°C", viernes: "10°C", sabado: "9°C", domingo: "11°C" },
+        { num: "11", nombre: "Alguna Parte, Alfa Centauro", ico: "🌥️", clima: "0°C", martes: "0°C", miercoles: "0°C", jueves: "0°C", viernes: "0°C", sabado: "0°C", domingo: "0°C" }
     ];
 
     lugares.forEach(lugar => {
@@ -23,36 +27,14 @@ $(document).ready(function () {
                 </div>
             </article>
         `;
+    });
 
-        $(`#btn-detalle-${lugar.num}`).click(function(){
+    lugares.forEach(lugar => {
+         $(`#btn-detalle-${lugar.num}`).click(function(){
             const lugarElegido = lugar;
             localStorage.setItem("lugarElegido", JSON.stringify(lugarElegido));
             window.location.href = 'detalle.html';
-
-            const detallesClimaCiudad = document.getElementById('lugares-clima');
-            const detallesCiudad = lugarElegido;
-        });   
+        }); 
     });
-
     
 });
-    
-    /*}); */
-    /*const detalleContent = document.getElementById('clima-detalle');
-    const detalles = [
-        { nombreDetalle: "Santiago, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Osorno, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Reñaca, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Panguipulli, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Toronto, Canadá", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Londres, Inglaterra", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
-        { nombreDetalle: "Melbourne, Australia", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" }
-    ];
-    
-    detalles.forEach(detalle => {
-        contenedor.innerHTML += `
-            <article class="card col-sm-12 col-md-4 mb-3">
-                <h1>Pronóstico de la semana: ${detalle.nombreDetalle}</h1>
-            </article>
-        `;
-    });*/
--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -8,6 +8,50 @@ $(document).ready(function () {
 
     detallesClimaCiudad.innerHTML += `
         <h1>Pronóstico de la semana: ${lugarDatos.nombre}</h1>
+
+            <div class="col-sm-12 col-md-4 text-center">
+                <h2>CLIMA</h2>
+                <div>${lugarDatos.ico}</div>
+                <h2>${lugarDatos.clima}</h2>
+            </div>
+
+            <div class="col-sm-12 col-md-4 text-center">
+                <h2>Humedad</h2>
+                <div>ICONO</div>
+                <h2>Número</h2>
+            </div>
+
+            <div class="col-sm-12 col-md-4 text-center">
+                <h2>Viento</h2>
+                <div>💨</div>
+                <h2>Número</h2>
+            </div>
+
+            <h3>Pronóstico Semana</h3>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Martes</h2>
+                <h3 class="card-text">${lugarDatos.martes}</h3>
+            </aside>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Miércoles</h2>
+                <h3 class="card-text">${lugarDatos.miercoles}</h3>
+            </aside>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Jueves</h2>
+                <h3 class="card-text">${lugarDatos.jueves}</h3>
+            </aside>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Viernes</h2>
+                <h3 class="card-text">${lugarDatos.viernes}</h3>
+            </aside>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Sábado</h2>
+                <h3 class="card-text">${lugarDatos.sabado}</h3>
+            </aside>
+            <aside class="card col-sm-12 col-md-4 card-body text-center">
+                <h2 class="card-title">Domingo</h2>
+                <h3 class="card-text">${lugarDatos.domingo}</h3>
+            </aside>
     `; 
 
     
