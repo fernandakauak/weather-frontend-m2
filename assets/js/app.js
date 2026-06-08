2121b2630e28fe16103598e5de5b79d4a2a60f5f
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-07 21:08:42 -0400

    Implementando interacción con detalle.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,65 @@
 $(document).ready(function () {
 
-    // Multiplicar cards sin repetir HTML
+    /* MULTIPLICAR CARD SIN REPETIR HTML */
     const contenedor = document.getElementById('lugares-clima');
     const lugares = [
-        {nombre: "Tu ciudad", clima: "18", url: "/detalle.html#1", lunes: "20", martes: "16" },
-        {nombre: "Osorno, Chile", clima: "7", url: "/detalle.html#2", lunes: "20", martes: "16" },
-        {nombre: "Reñaca, Chile", clima: "15", url: "/detalle.html#3", lunes: "20", martes: "16" },
-        {nombre: "Panguipulli, Chile", clima: "10", url: "/detalle.html#4", lunes: "20", martes: "16" },
-        {nombre: "Toronto, Canadá", clima: "10", url: "/detalle.html#5", lunes: "20", martes: "16" },
-        {nombre: "Londres, Inglaterra", clima: "20", url: "/detalle.html#6", lunes: "20", martes: "16" },
-        {nombre: "Melbourne, Australia", clima: "25", url: "/detalle.html#7", lunes: "20", martes: "16" }
+        { nombre: "Santiago, Chile", clima: "18", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombre: "Osorno, Chile", clima: "7", lunes: "20", martes: "16" },
+        { nombre: "Reñaca, Chile", clima: "15", lunes: "20", martes: "16" },
+        { nombre: "Panguipulli, Chile", clima: "10", lunes: "20", martes: "16" },
+        { nombre: "Toronto, Canadá", clima: "10", lunes: "20", martes: "16" },
+        { nombre: "Londres, Inglaterra", clima: "20", lunes: "20", martes: "16" },
+        { nombre: "Melbourne, Australia", clima: "25", lunes: "20", martes: "16" }
     ];
-
-    contenedor.innerHTML = '';
 
     lugares.forEach(lugar => {
         contenedor.innerHTML += `
             <article class="card col-sm-12 col-md-4 mb-3">
-                
                 <div class="card-body">
                     <h5 class="card-title">${lugar.nombre}</h5>
                     <p class="card-text">${lugar.clima}</p>
-                    <img src="assets/img/prueba.png" class="card-img-top mt-3" alt="Imagen de Lugar">
-                    <a type="button" class="btn btn-primary class-12" href="${lugar.url}">Detalles</a>
+                    <a type="button" class="btn btn-primary class-12 btn-detalle" href="#">Detalles</a>
                 </div>
             </article>
         `;
+
+        $(".btn-detalle").click(function(){
+            const lugarElegido = lugar;
+            JSON.parse(localStorage.getItem(lugarElegido));
+            window.location.href = 'detalle.html';
+
+            const detallesClimaCiudad = document.getElementById('lugares-clima');
+            const detallesCiudad = lugarElegido;
+
+            lugares.forEach(lugar => {
+                detallesClimaCiudad.innerHTML += `
+                    <article class="card col-sm-12 col-md-4 mb-3">
+                        <h1>Pronóstico de la semana: ${lugar.nombreDetalle}</h1>
+                    </article>
+                `;
+            }); 
+        });   
     });
 
+    
 });
+    
+    /*}); */
+    /*const detalleContent = document.getElementById('clima-detalle');
+    const detalles = [
+        { nombreDetalle: "Santiago, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Osorno, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Reñaca, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Panguipulli, Chile", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Toronto, Canadá", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Londres, Inglaterra", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" },
+        { nombreDetalle: "Melbourne, Australia", lunes: "18", martes: "20", miercoles: "16", jueves: "18", viernes: "20", sabado: "16", domingo: "16" }
+    ];
+    
+    detalles.forEach(detalle => {
+        contenedor.innerHTML += `
+            <article class="card col-sm-12 col-md-4 mb-3">
+                <h1>Pronóstico de la semana: ${detalle.nombreDetalle}</h1>
+            </article>
+        `;
+    });*/
