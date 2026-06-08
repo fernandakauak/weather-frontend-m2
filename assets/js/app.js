148243256f627c237121033b8066701400ddac4a
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-06-07 23:23:44 -0400

    Cambios en CSS e interacción detalle

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     /* MULTIPLICAR CARD SIN REPETIR HTML */
     const contenedor = document.getElementById('lugares-clima');
     const lugares = [
-        { nombre: "Santiago, Chile", ico: "☁️", clima: "18°C", lunes: "18°C", martes: "20°C", miercoles: "16°C", jueves: "18°C", viernes: "20°C", sabado: "16°C", domingo: "16°C" },
-        { nombre: "Osorno, Chile", ico: "🌧️", clima: "7°C", lunes: "20", martes: "16" },
-        { nombre: "Reñaca, Chile", ico: "⛅", clima: "15°C", lunes: "20", martes: "16" },
-        { nombre: "Panguipulli, Chile", ico: "🌧️", clima: "10°C", lunes: "20", martes: "16" },
-        { nombre: "Toronto, Canadá", ico: "⛅", clima: "10°C", lunes: "20", martes: "16" },
-        { nombre: "Londres, Inglaterra", ico: "🌧️", clima: "20°C", lunes: "20", martes: "16" },
-        { nombre: "Melbourne, Australia", ico: "☀️", clima: "25°C", lunes: "20", martes: "16" }
+        { num: "1", nombre: "Santiago, Chile", ico: "☁️", clima: "18°C", lunes: "18°C", martes: "20°C", miercoles: "16°C", jueves: "18°C", viernes: "20°C", sabado: "16°C", domingo: "16°C" },
+        { num: "2", nombre: "Osorno, Chile", ico: "🌧️", clima: "7°C", lunes: "20", martes: "16" },
+        { num: "3", nombre: "Reñaca, Chile", ico: "⛅", clima: "15°C", lunes: "20", martes: "16" },
+        { num: "4", nombre: "Panguipulli, Chile", ico: "🌧️", clima: "10°C", lunes: "20", martes: "16" },
+        { num: "5", nombre: "Toronto, Canadá", ico: "⛅", clima: "10°C", lunes: "20", martes: "16" },
+        { num: "6", nombre: "Londres, Inglaterra", ico: "🌧️", clima: "20°C", lunes: "20", martes: "16" },
+        { num: "7", nombre: "Melbourne, Australia", ico: "☀️", clima: "25°C", lunes: "20", martes: "16" }
     ];
 
     lugares.forEach(lugar => {
@@ -19,26 +19,18 @@ $(document).ready(function () {
                     <h2 class="card-title">${lugar.nombre}</h2>
                     <div class="clima-ico">${lugar.ico}</div>
                     <h3 class="card-text">${lugar.clima}</h3>
-                    <a type="button" class="btn btn-primary class-12 btn-detalle" href="#">Detalles</a>
+                    <a type="button" class="btn btn-primary class-12" id="btn-detalle-${lugar.num}" href="#">Detalles</a>
                 </div>
             </article>
         `;
 
-        $(".btn-detalle").click(function(){
+        $(`#btn-detalle-${lugar.num}`).click(function(){
             const lugarElegido = lugar;
             localStorage.setItem("lugarElegido", JSON.stringify(lugarElegido));
             window.location.href = 'detalle.html';
 
             const detallesClimaCiudad = document.getElementById('lugares-clima');
             const detallesCiudad = lugarElegido;
-
-            lugares.forEach(lugar => {
-                detallesClimaCiudad.innerHTML += `
-                    <article class="card col-sm-12 col-md-4 mb-3 detalle">
-                        <h1>Pronóstico de la semana: ${lugar.nombreDetalle}</h1>
-                    </article>
-                `;
-            }); 
         });   
     });
 
--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -1,22 +1,14 @@
 $(document).ready(function () {
 
-    $(".btn-detalle").click(function(){
-        const lugarElegido = lugar;
-        localStorage.setItem("lugarElegido", JSON.stringify(lugarElegido));
-        window.location.href = 'detalle.html';
-        
-        const lugarDatos = JSON.parse(lugarElegido);
-        const detallesClimaCiudad = document.getElementById('clima-detalle');
-        const detallesCiudad = lugarElegido;
+    const lugarElegido = localStorage.getItem("lugarElegido");
 
-        lugarDatos.forEach(lugar => {
-            detallesClimaCiudad.innerHTML += `
-                <article class="card col-sm-12 col-md-4 mb-3 detalle">
-                    <h1>Pronóstico de la semana: ${lugar.nombreDetalle}</h1>
-                </article>
-            `;
-        }); 
-    }); 
+    const lugarDatos = JSON.parse(lugarElegido);
+    const detallesClimaCiudad = document.getElementById('clima-detalle');
+    const detallesCiudad = lugarElegido;
+
+    detallesClimaCiudad.innerHTML += `
+        <h1>Pronóstico de la semana: ${lugarDatos.nombre}</h1>
+    `; 
 
     
 });
