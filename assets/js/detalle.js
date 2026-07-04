fd286d0ac69f8cfa1fd2dd227319e80c9f442da9
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-04 16:44:15 -0400

    Mejora interfaz de detalle y visualización de datos nuevos (promedios, clima más repetido y respuesta)

--- assets/js/detalle.js
+++ assets/js/detalle.js
@@ -7,16 +7,33 @@ $(document).ready(function () {
     const detallesEstadisticaSemana = document.getElementById('estadistica-semana');
     const detallesCiudad = lugarElegido;
 
-    /* PROMEDIOS: MÍNIMO, MÁXIMO Y TODO */
-    let promedioMin = Math.round((parseInt(lugarDatos.pronosticoSemanal[0].min) + parseInt(lugarDatos.pronosticoSemanal[1].min) + parseInt(lugarDatos.pronosticoSemanal[2].min)) / 3);
+    /* PROMEDIO MÍNIMO */
+    let promedioMin = [parseInt(lugarDatos.pronosticoSemanal[0].min), parseInt(lugarDatos.pronosticoSemanal[1].min), parseInt(lugarDatos.pronosticoSemanal[2].min), parseInt(lugarDatos.pronosticoSemanal[3].min), parseInt(lugarDatos.pronosticoSemanal[4].min), parseInt(lugarDatos.pronosticoSemanal[5].min)];
 
-    let promedioMax  = Math.round((parseInt(lugarDatos.pronosticoSemanal[0].max) + parseInt(lugarDatos.pronosticoSemanal[1].max) + parseInt(lugarDatos.pronosticoSemanal[2].max)) / 3);
+    let sum = 0;
 
-    let promedioTodo = Math.round((promedioMin + promedioMax) / 2);
+    for (let i = 0; i < promedioMin.length; i++) {
+        sum += promedioMin[i];
+    }
 
+    const promedioMinCiudad = Math.round(sum / promedioMin.length);
+
+    /* PROMEDIO MÁXIMO */
+    let promedioMax = [parseInt(lugarDatos.pronosticoSemanal[0].max), parseInt(lugarDatos.pronosticoSemanal[1].max), parseInt(lugarDatos.pronosticoSemanal[2].max), parseInt(lugarDatos.pronosticoSemanal[3].max), parseInt(lugarDatos.pronosticoSemanal[4].max), parseInt(lugarDatos.pronosticoSemanal[5].max)];
+
+    for (let i = 0; i < promedioMax.length; i++) {
+        sum += promedioMax[i];
+    }
+
+    const promedioMaxCiudad = Math.round(sum / promedioMax.length);
+
+
+    /* PROMEDIO MÍNIMO Y MÁXIMO */
+    let promedioTodoCiudad = Math.round((promedioMinCiudad + promedioMaxCiudad) / 2);
+
+
+    /* MODA CLIMA POR CIUDAD */
     let estadoClimaCiudad = [lugarDatos.pronosticoSemanal[0].estado, lugarDatos.pronosticoSemanal[1].estado, lugarDatos.pronosticoSemanal[2].estado, lugarDatos.pronosticoSemanal[3].estado, lugarDatos.pronosticoSemanal[4].estado, lugarDatos.pronosticoSemanal[5].estado];
-
-    console.log(estadoClimaCiudad);
 
     function modaClima(arr){
         return arr.sort((a,b) =>
@@ -27,23 +44,23 @@ $(document).ready(function () {
 
     let modaClimaCiudad = modaClima(estadoClimaCiudad);
 
+
+    /* MENSAJE SEGÚN CLIMA */
     function resumenClima() {
         if (modaClimaCiudad === "Soleado"){
-            return `☀️ Como calienta el sol en tu ciudad`
+            return `☀️ Como calienta el sol en tu ciudad... 🎵`
         } else if (modaClimaCiudad === "Nubosidad Parcial"){
-            return `⛅ Algo tranquilo para esta temporada`
+            return `⛅ Un poco de nube, un poco de sol, un poco de todo, a little respect to me... 🎵`
         } else if (modaClimaCiudad === "Nublado") {
-            return `☁️ Está nubladito`
+            return `☁️ Está nubladito, abrígate y da un paseo un ratito, little fluffy clouds 🎵`
         } else if (modaClimaCiudad === "Chubascos"){
-            return `🌧️ Esta tarde vi llover, vi gente correr...`
+            return `🌧️ Esta tarde vi llover, vi gente correr... 🎵`
         } else {
-            return `🪐 No sé tú pero yo no sé donde estamos.`
+            return `🪐 No sé tú pero yo no sé donde estamos... this is Major Tom to Ground Control 🎵`
         }
     }
-    
 
     let resumenClimaCiudad = resumenClima()
-    console.log(resumenClimaCiudad);
 
 
     /* COLOCAR EN ESTRUCTURA HTML */
@@ -73,53 +90,62 @@ $(document).ready(function () {
         <h2 class="container__title">Pronóstico Semana</h2>
         <h3 class="container__promedio">Promedio de máxima de la semana: ${lugarDatos.nombre}</h3>
 
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[0].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[0].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[0].min} / ${lugarDatos.pronosticoSemanal[0].max}</h3>
-        </aside>
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        </article>
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[1].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[1].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[1].min} / ${lugarDatos.pronosticoSemanal[1].max}</h3>
-        </aside>
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        </article>
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[2].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[2].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[2].min} / ${lugarDatos.pronosticoSemanal[2].max}</h3>
-        </aside>
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        </article>
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[3].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[3].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[3].min} / ${lugarDatos.pronosticoSemanal[3].max}</h3>
-        </aside>
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        </article>
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[4].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[4].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[4].min} / ${lugarDatos.pronosticoSemanal[4].max}</h3>
-        </aside>
-        <aside class="card col-sm-12 col-md-4 card-body forecast__card">
+        </article>
+        <article class="card col-sm-12 col-md-4 card-body forecast__card">
             <h3 class="forecast__title">${lugarDatos.pronosticoSemanal[5].dia}</h3>
             <h3 class="forecast__state">${lugarDatos.pronosticoSemanal[5].estado}</h3>
             <h3 class="forecast__grade">${lugarDatos.pronosticoSemanal[5].min} / ${lugarDatos.pronosticoSemanal[5].max}</h3>
-        </aside>
+        </article>
     `; 
 
     detallesEstadisticaSemana.innerHTML += `
-        <h3 class="container__promedio">Promedio de máxima de la semana: ${lugarDatos.nombre}</h3>
+    <h3 class="promedio__title">Promedio de máxima de la semana: ${lugarDatos.nombre}</h3>
 
-        <aside class="summary__card">
-            <h3 class="summary__state">Mínimo de la semana: ${promedioMin}</h3>
-            <h3 class="summary__state">Máximo de la semana: ${promedioMax}</h3>
-            <h3 class="summary__grade">Promedio de la semana: ${promedioTodo}</h3>
-        </aside>
+    <div class="col-sm-12 promedio__section">
+        <article class="promedio__min col-sm-12 col-md-4 ">
+            <h3 class="grado__title">Mínimo de la semana:</h3>
+            <h4 class="grado__text">${promedioMinCiudad}</h4>
+        </article>
+        <article class="promedio__max col-sm-12 col-md-4 ">
+            <h3 class="grado__title">Máximo de la semana:</h3>
+            <h4 class="grado__text">${promedioMaxCiudad}</h4>
+        </article>
+        <article class="promedio__todo col-sm-12 col-md-4 ">
+            <h3 class="grado__title">Promedio de la semana:</h3>
+            <h4 class="grado__text">${promedioTodoCiudad}</h4>
+        </article>
+    </div>
 
-        <aside class="summary__card">
-            <h3 class="summary__state">Clima más repetido: ${modaClimaCiudad}</h3>
-            <h3 class="summary__state">Consideración para este clima: ${resumenClimaCiudad}</h3>           
-        </aside>
-
-
-        
+    <aside class="promedio__estado">
+        <h3 class="estado__title">Clima más repetido:</h3>
+        <h4 class="estado__text">${modaClimaCiudad}</h4>
+        <h3 class="estado__title">Consideración para este clima:</h3> 
+        <h4 class="estado__text">${resumenClimaCiudad}</h4>          
+    </aside>
     `; 
+
 });
